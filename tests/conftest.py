ba2340b7a7d9735a599ab8dbe7c538b990c394d9
Andre Koraleski <andrekorale@gmail.com>
2025-06-18 01:40:51 -0300

    Fix: Now the test doesn't do repeat pizzas

--- tests/conftest.py
+++ tests/conftest.py
@@ -19,13 +19,15 @@ class MockPolicy(PlayerController):
 def game_state(request) -> GameState:
     """Fixture to create a GameState, parameterized by the number of players."""
     num_players = request.param
-    
     recipes_per_player = {2: 3, 3: 2, 6: 1}[num_players]
     
+    # Create an iterator to deal the pizzas without repetition
+    pizza_dealer = iter(ALL_PIZZAS)
     all_recipes = [
-        [ALL_PIZZAS[j % len(ALL_PIZZAS)] for j in range(recipes_per_player)]
-        for i in range(num_players)
+        [next(pizza_dealer) for _ in range(recipes_per_player)]
+        for _ in range(num_players)
     ]
+    
     controllers: List[PlayerController] = [MockPolicy() for _ in range(num_players)]
     
     return GameState(
